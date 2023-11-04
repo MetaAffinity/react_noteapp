a4c61beb930cc8c284ea3ff2b53b5ba261cba510
MetaAffinity <metaaffinity@gmail.com>
2023-11-04 06:08:34 +0500

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -121,7 +121,7 @@ const Home = () => {
           <textarea value={description} onChange={(e) => setDescription(e.target.value)}  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" id="comment" placeholder="Enter your comment" name="comment" rows="5" cols="40" />
         </div>
         <button disabled={loading} className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Add Note</button>
-        <p className="text-xs text-gray-500 mt-3">Literally you probably havent heard of them jean shorts.</p>
+        
         </form>
       </div>
 
