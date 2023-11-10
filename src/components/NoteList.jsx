1924f994baf3d22d1f0cdc69afcd3500980eb474
MetaAffinity <metaaffinity@gmail.com>
2023-11-11 02:20:06 +0500

    update

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -6,7 +6,7 @@ const NoteList = ({title,description, createdAt, user, deleteHandler,updateHandl
     <>
         <div className="w-full p-8 md:w-1/2 lg:w-1/3      shadow-xl">
             <div className="flex items-center mb-6">
-                <svg  width="20" height="20" fill="currentColor" className="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                <svg onClick={() => updateHandler(id)} style={{cursor:"pointer"}}  width="20" height="20" fill="currentColor" className="w-6 h-6 text-indigo-500" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                     <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
                     </path>
                 </svg>
@@ -20,7 +20,8 @@ const NoteList = ({title,description, createdAt, user, deleteHandler,updateHandl
             <p>{createdAt}</p>
             <div className="flex items-center mt-4">{user}</div>
             <button  onClick={() => deleteHandler(id)} className="text-white text-base  py-1 px-3 bg-sky-500 hover:bg-sky-700 rounded">Delete</button>
-            <button  onClick={() => updateHandler(title, description, id)} className="text-white text-base  py-1 px-3 bg-sky-500 hover:bg-sky-700 rounded">Delete</button>
+            <span className='m-0.5'></span>
+            <button  onClick={() => updateHandler(id, title, description)} className="text-white text-base  py-1 px-3 bg-sky-500 hover:bg-sky-700 rounded">Update</button>
         </div>
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -84,7 +84,7 @@ const Home = () => {
 
   const saveUpdateHandler  = async() => {
       try {
-            const {data} = await axios.put(`${server}/notes/update/${idToUpdate}`, {
+            const {data} = await axios.put(`${server}/notes/${idToUpdate}`, {
               title,description
             },{
               headers:{
@@ -93,6 +93,7 @@ const Home = () => {
               withCredentials:true,
             })
             toast.success(data.message)
+            setReload(prev=>!prev)
             showUpdate(false)
       } catch (error) {
         setShowUpdate(false)
@@ -234,27 +235,49 @@ const Home = () => {
 
         {/* update popup */}
         {showUpdate ? (
-        <>
+                  <>
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-              <div className="relative p-6 flex-auto">
-                <input
-                  type="text"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                  className="mb-4 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Update Title"
-                />
-                <textarea
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                  placeholder="Update Description"
-                  rows="5"
-                  cols="40"
-                />
-              </div>
+            <div className="relative w-auto my-6 mx-auto max-w-sm">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Update
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowUpdate(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <div className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                        <div className="relative p-6 flex-auto">
+                      <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        className="mb-4 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        placeholder="Update Title"
+                      />
+                      <textarea
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                        placeholder="Update Description"
+                        rows="5"
+                        cols="40"
+                      />
+                    </div>
+                  </div>
+                </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
@@ -272,9 +295,12 @@ const Home = () => {
                     Save Changes
                   </button>
                 </div>
+              </div>
+            </div>
           </div>
           <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
         </>
+        
       ) : null}
 
 
